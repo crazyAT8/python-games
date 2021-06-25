634268ea1f57a06953083817ebe1d1b0cdb022dc
crazyAT8 <80292649+crazyAT8@users.noreply.github.com>
2021-06-25 04:19:47 -0500

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,9 @@
+# the computer not the user chooses the word, the user has to guess the word
+# getting the computer to choose a random english word
+
+import random
+from words import words
+
+
+
+
